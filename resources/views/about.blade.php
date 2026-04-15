77f0f41950e99d2ff35704d6ed6a3f6bb38d509c
Hugo MARTIN <hugo.martin@done.fr>
2026-04-15 11:34:44 +0200

    Initial commit: Hello World

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>About us</h1>
+
+    <a href="/">Return home</a>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Hello world!</h1>
+
+    <a href="/about">About us</a>
+</body>
+</html>
