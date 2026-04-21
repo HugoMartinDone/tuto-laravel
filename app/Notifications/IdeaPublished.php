38245a2f6b5b6434558ad9cefa6c2973751ec616
Hugo MARTIN <hugo.martin@done.fr>
2026-04-21 12:24:08 +0200

    feat: added mail notification on idea create

---
+++ app/Notifications/IdeaPublished.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use App\Models\Idea;
+
+class IdeaPublished extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(private Idea $idea)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        $url = url('/ideas/' . $this->idea->id);
+        return (new MailMessage)
+            ->greeting('Hi!')
+            ->line('You published a new idea.')
+            ->action('Read it!', $url)
+            ->line('Thanks for using our app!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            //
+        ];
+    }
+}
